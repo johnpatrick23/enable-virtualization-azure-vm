823735a2cbea95c4a7bbdd535cf26b9d9bf4519d
johnpatrick23 <johnpatrickpapares@gmail.com>
2021-12-13 15:51:37 +0800

    Initial Commit

---
+++ enable_virtualization.ps1
@@ -0,0 +1,21 @@
+﻿$downloadZipUrl = "https://download.microsoft.com/download/B/D/8/BD821B1F-05F2-4A7E-AA03-DF6C4F687B07/dgreadiness_v3.6.zip"
+
+$downloadPath = "$HOME\Downloads\dgreadiness_v3.6.zip"
+$unzipPath = "$HOME\Downloads"
+
+Invoke-WebRequest -Uri $downloadZipUrl -OutFile $downloadPath
+
+Expand-Archive -LiteralPath $downloadPath -DestinationPath $unzipPath
+
+
+echo R | &"$unzipPath\dgreadiness_v3.6\DG_Readiness_Tool_v3.6.ps1" -enable
+
+
+
+echo R | &"$unzipPath\dgreadiness_v3.6\DG_Readiness_Tool_v3.6.ps1" -disable
+
+cmd.exe /c "bcdedit /deletevalue hypervisorlaunchtype"
+
+cmd.exe /c "bcdedit /set hypervisorlaunchtype off"
+
+cmd.exe /c "shutdown /r /t 000"
